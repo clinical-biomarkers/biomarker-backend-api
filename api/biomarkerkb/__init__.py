804edc43934a85dab96311f77fbbe10877df90be
skim658 <33474168+seankim658@users.noreply.github.com>
2023-09-26 17:33:06 -0400

    add configuration file logic

--- api/biomarkerkb/__init__.py
+++ api/biomarkerkb/__init__.py
@@ -1,27 +1,26 @@
-import os 
 from flask import Flask 
-from flask_jwt_extended import JWTManager
 from flask_cors import CORS 
 from flask_restx import Api
-from .dataset import api as dataset_api 
-from flask_pymongo import PyMongo
+from dataset import api as dataset_api 
+from db import init_app as init_db_app
 
 def create_app():
 
     # create flask instance 
     app = Flask(__name__, instance_relative_config = True)
 
-    # load configurations  
-    # TODO : figure out how to deal with these 
-    app.config['MONGO_URI'] = f'mongodb://running_biomarkerkb:27017/biomarkerkbdb'
-    app.config['JWT_SECRET_KEY'] = f''
-
     CORS(app)
-    jwt = JWTManager(app)
-    mongo = PyMongo(app)
+    init_db_app(app)
 
     # setup the api using the flask_restx library 
     api = Api(app, version = '1.0', title = 'BiomarkerKB APIs', description = 'Biomarker Knowledgebase API')
     api.add_namespace(dataset_api)
+
+    app.config['ENV'] = 'development'
+
+    if app.config['ENV'] == 'production':
+        app.config.from_pyfile('/config/config.py')
+    else:
+        app.config.from_pyfile('/config/config_dev.py')
 
     return app
