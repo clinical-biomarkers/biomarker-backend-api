a847c4f08c1d03d4f67e62bb7a472851ec776b8b
skim658 <33474168+seankim658@users.noreply.github.com>
2023-10-01 21:16:08 -0400

    added auth mechanic to conn string

--- api/biomarkerkb/config/config.py
+++ api/biomarkerkb/config/config.py
@@ -2,7 +2,9 @@ DEBUG = False
 TESTING = False 
 
 SERVER = 'prd'
+DB_AUTH = 'SCRAM-SHA-1'
 DB_NAME = 'biomarkerkbdb'
+DB_COLLECTION = 'biomarker_collection'
 DB_USERNAME = 'biomarkeradmin'
 DB_PASSWORD = 'biomarkerpass'
-MONGO_URI = f'mongodb://{DB_USERNAME}:{DB_PASSWORD}@localhost:27017/{DB_NAME}?authSource={DB_NAME}'
+MONGO_URI = f'mongodb://{DB_USERNAME}:{DB_PASSWORD}@localhost:27017/?authMechanism={DB_AUTH}&authSource={DB_NAME}'
--- api/biomarkerkb/config/config_dev.py
+++ api/biomarkerkb/config/config_dev.py
@@ -2,8 +2,9 @@ DEBUG = True
 TESTING = False 
 
 SERVER = 'dev'
+DB_AUTH = 'SCRAM-SHA-1'
 DB_NAME = 'biomarkerkbdb'
 DB_COLLECTION = 'biomarker_collection'
 DB_USERNAME = 'biomarkeradmin'
 DB_PASSWORD = 'biomarkerpass'
-MONGO_URI = f'mongodb://{DB_USERNAME}:{DB_PASSWORD}@localhost:27017/{DB_NAME}?authSource={DB_NAME}'
+MONGO_URI = f'mongodb://{DB_USERNAME}:{DB_PASSWORD}@localhost:27017/?authMechanism={DB_AUTH}&authSource={DB_NAME}'
