ed6814a71a0477d226f90d32acdc836b0c239f43
skim658 <33474168+seankim658@users.noreply.github.com>
2023-10-01 21:16:33 -0400

    fixed config set order

--- api/biomarkerkb/__init__.py
+++ api/biomarkerkb/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask 
 from flask_cors import CORS 
 from flask_restx import Api
-from dataset import api as dataset_api 
-from db import init_app as init_db_app
+from .dataset import api as dataset_api 
+from .db import init_app as init_db_app
 
 def create_app():
 
     # create flask instance 
-    app = Flask(__name__, instance_relative_config = True)
+    app = Flask(__name__)
+
+    app.config['ENV'] = 'development'
+
+    if app.config['ENV'] == 'production':
+        app.config.from_pyfile('./config/config.py')
+    else:
+        app.config.from_pyfile('./config/config_dev.py')
 
     CORS(app)
     init_db_app(app)
@@ -15,12 +22,5 @@ def create_app():
     # setup the api using the flask_restx library 
     api = Api(app, version = '1.0', title = 'BiomarkerKB APIs', description = 'Biomarker Knowledgebase API')
     api.add_namespace(dataset_api)
-
-    app.config['ENV'] = 'development'
-
-    if app.config['ENV'] == 'production':
-        app.config.from_pyfile('/config/config.py')
-    else:
-        app.config.from_pyfile('/config/config_dev.py')
 
     return app
