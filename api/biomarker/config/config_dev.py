d6c7bc2ae7dc1624023244523f72ab1dabf06455
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-01-05 14:08:42 -0500

    add id collection and db name fix

--- api/biomarker/config/config_dev.py
+++ api/biomarker/config/config_dev.py
@@ -3,8 +3,9 @@ TESTING = False
 
 SERVER = 'dev'
 DB_AUTH = 'SCRAM-SHA-1'
-DB_NAME = 'biomarkerkbdb'
+DB_NAME = 'biomarkerdb_api'
 DB_COLLECTION = 'biomarker_collection'
+ID_COLLECTION = 'id_map_collection'
 DB_USERNAME = 'biomarkeradmin'
 DB_PASSWORD = 'biomarkerpass'
 MONGO_URI = f'mongodb://{DB_USERNAME}:{DB_PASSWORD}@172.18.0.2:27017/{DB_NAME}?authMechanism={DB_AUTH}&authSource={DB_NAME}'
