a835956730b3404474a7e746b316148b0fa5effc
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-01-18 20:34:09 -0500

    update package name to match dockerfile

--- api/setup.py
+++ api/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 def main():
 
-    setup(name = 'biomarkerkb',
+    setup(name = 'biomarker',
           version = '1.0',
           packages = find_packages(),
           include_package_data = True, 
