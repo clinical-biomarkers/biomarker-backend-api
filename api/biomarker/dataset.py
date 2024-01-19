9b3136d08c1d950bb5b22c22602bcd914923e568
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-01-18 19:38:49 -0500

    add better error handling

--- api/biomarker/dataset.py
+++ api/biomarker/dataset.py
@@ -1,9 +1,9 @@
-from flask_restx import Namespace, Resource, fields
+from flask_restx import Namespace, Resource, inputs
 from flask import current_app as app 
 from flask import request
 from data_models import data_model
 
-api = Namespace ('dataset', description = 'Dataset operations API')
+api = Namespace('dataset', description = 'Dataset operations API')
 
 def get_collection_name():
     return app.config['DB_COLLECTION']
@@ -11,25 +11,28 @@ def get_collection_name():
 class DatasetGetAll(Resource):
     ''' Get the entire dataset. 
     '''
-    @api.doc(description = 'Returns all the data records.')
+    @api.doc(description = 'Returns all the data records Supports pagination and per page filtering.')
+    @api.param('page', 'The page number to return.', type = int, default = 1)
+    @api.param('per_page', 'The number of records to return per page.', type = int, default = 50)
     @api.response(200, 'Success', data_model)
     def get(self):
-        data = app.mongo.db[get_collection_name()].find() 
+        page = request.args.get('page', default = 1, type = int)
+        per_page = request.args.get('per_page', default = 50, type = int)
+        data = app.mongo.db[get_collection_name()].find().skip((page - 1) * per_page).limit(per_page)
         return list(data)
 
 class DatasetRandomSample(Resource):
     ''' Get a random subset of data. 
     '''
-    @api.doc(description = 'Returns a random subset of the data.')
-    @api.param('sample', 'The number of samples to return.')
+    @api.doc(description = 'Returns a random subset of the data. The sample size must be a positive integer.')
+    @api.param('sample', 'The number of samples to return.', type = inputs.positive, default = 1)
     @api.response(200, 'Success', data_model)
+    @api.response(400, 'Bad Request')
     def get(self):
         try:
-            sample_size = int(request.args.get('sample', default = 1))
+            sample_size = request.args.get('sample', default = 1, type = int)
         except ValueError:
-            return {'message': 'Invalid sample size provided'}, 400
-        if sample_size <= 0:
-            return {'message': 'Sample size must be a positive integer.'}, 400
+            return {'message': 'Invalid sample size provided. Sample must be a positive integer.'}, 400
         data = app.mongo.db[get_collection_name()].aggregate([{'$sample': {'size': sample_size}}])
         return list(data)
 
