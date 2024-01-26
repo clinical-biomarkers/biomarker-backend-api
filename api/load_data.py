aa37eb50d8a79cd90eed5d74e7aae51f479060a6
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-01-26 12:50:14 -0500

    handle prod replica of ID collection map

--- api/load_data.py
+++ api/load_data.py
@@ -89,7 +89,36 @@ def process_data(data: dict | list, dbh, db_collection: str, fp: str) -> bool:
             return False 
     
     return True 
-    
+
+def load_id_collection(id_collection_data: list, dbh, id_collection: str) -> None:
+    ''' Loads the id_collection.json file into the prod database.
+
+    Parameters
+    ----------
+    id_collection_data: list
+        The data to load into the id_collection.
+    dbh: pymongo.MongoClient
+        The database handle.
+    id_collection: str
+        The name of the collection to insert the data into.
+    '''
+    if not id_collection_data:
+        logging.error(f'No data found for id_collection_data.')
+        print(f'No data found for id_collection_data.')
+        return
+    misc_fns.setup_index(dbh, 'hash_value', id_collection, 'hash_value_1') 
+    bulk_ops = [pymongo.InsertOne(document) for document in id_collection_data]
+    try:
+        if bulk_ops:
+            dbh[id_collection].bulk_write(bulk_ops, ordered = False)
+            logging.info(f'Successfully loaded id_collection_data.')
+            print(f'Successfully loaded id_collection_data.')
+    except DuplicateKeyError as e:
+        pass
+    except Exception as e:
+        logging.error(f'Error loading id_collection_data into prod database: {e}.') 
+        print(f'Error loading id_collection_data into prod database: {e}.')
+
 def main(): 
     
     ### handle command line arguments
@@ -98,13 +127,11 @@ def main():
         usage = 'python load_data.py [options] server ver'
     )
     parser.add_argument('-s', '--server', help = 'tst/prd')
-    parser.add_argument('-v', '--ver', help = 'data release version')
     options = parser.parse_args()
     if not options.server or not options.ver:
         parser.print_help()
         sys.exit(1)
     server = options.server
-    data_ver = options.ver
 
     ### get config info
     config_obj = misc_fns.load_json('config.json')
@@ -113,6 +140,7 @@ def main():
     db_name = config_obj['dbinfo']['dbname']
     data_root_path = config_obj['data_path']
     db_collection = config_obj['dbinfo'][db_name]['collection']
+    id_collection = config_obj['dbinfo'][db_name]['id_collection']
     db_user = config_obj['dbinfo'][db_name]['user']
     db_pass = config_obj['dbinfo'][db_name]['password']
     # get the database handle
@@ -120,13 +148,18 @@ def main():
 
     ### setup logger
     misc_fns.setup_logging(f'./logs/load_data_{server}.log')
-    logging.info(f'Loading data for server: {server} and data release version: {data_ver}. #####################')
+    logging.info(f'Loading data for server: {server}. #####################')
 
     ### setup first run biomarker_id index 
     misc_fns.setup_index(dbh, 'biomarker_id', db_collection, 'biomarker_id_1')
     
     ### begin processing data 
     data_release_glob_pattern = f'{data_root_path}/generated/datamodel/new_data/current/*.json'
+    # if running on prd server, load the id_collection.json file
+    if server == 'prd':
+        id_collection_json_path = f'{data_root_path}/generated/datamodel/id_collection.json'
+        load_id_collection(misc_fns.load_json(id_collection_json_path), dbh, id_collection)
+
     for fp in glob.glob(data_release_glob_pattern):
         data = misc_fns.load_json(fp)
         if process_data(data, dbh, db_collection, fp):
@@ -136,7 +169,7 @@ def main():
             logging.error(f'Failed to load data for file: {fp}.')
             print(f'Failed to load data for file: {fp}.')
 
-    logging.info(f'Finished loading data for server: {server} and data release version: {data_ver}. ---------------------')
+    logging.info(f'Finished loading data for server: {server}. ---------------------')
 
 if __name__ == '__main__': 
     main() 
