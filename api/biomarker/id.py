e8ad7e9dd084b7697b09ea832013fba56132293b
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-02-14 16:41:07 -0500

    simplify endpoint and add set endpoint

--- api/biomarker/id.py
+++ api/biomarker/id.py
@@ -12,12 +12,12 @@ class Biomarker(Resource):
     ''' Get a single biomarker record by biomarker ID. 
     '''
     @api.doc(description = 'Returns a single biomarker record by biomarker ID.')
-    @api.response(200, 'Success', data_model)
+    @api.response(200, 'Partial or full success', data_model)
     @api.response(400, 'No biomarker ID provided')
     @api.response(404, 'Not Found')
-    @api.param('biomarker_id', 'The biomarker ID.', type = str, required = True)
-    def get(self):
-        biomarker_id = request.args.get('biomarker_id', default = None)
+    @api.param('biomarker_id', 'The biomarker ID.', _in = 'path', type = str, required = True)
+    def get(self, id):
+        biomarker_id = id
         if not biomarker_id:
             return {'message': 'No biomarker ID provided'}, 400
         try:
@@ -28,4 +28,27 @@ class Biomarker(Resource):
             return {'message': 'Biomarker ID not found'}, 404
         return data
 
-api.add_resource(Biomarker, '/getbiomarker')
+class Biomarkers(Resource):
+    ''' Get multiple biomarker records by biomarker ID. 
+    '''
+    @api.doc(description = 'Returns multiple biomarker records by biomarker ID.')
+    @api.response(200, 'Success', data_model)
+    @api.response(400, 'No biomarker IDs provided')
+    @api.response(404, 'Not Found')
+    def post(self):
+        data = request.json
+        biomarker_ids = data.get('biomarker_ids')
+
+        if not biomarker_ids:
+            return {'message': 'No biomarker IDs provided'}, 400
+
+        try:
+            results = app.mongo_db[get_collection_name()].find({'biomarker_id': {'$in': biomarker_ids}}, {'_id': 0})
+            if results.count() == 0:
+                return {'message': 'Biomarker IDs not found'}, 404
+            return list(results), 200
+        except Exception as e:
+            return {'message': 'Error processing request'}, 400
+
+api.add_resource(Biomarker, '/<string:id>')
+api.add_resource(Biomarkers, '/set')
