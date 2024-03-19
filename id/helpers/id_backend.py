06df47309635ec81d7f41173e6efc53c7524ea83
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-03-19 15:52:39 -0400

    id backend layer

--- id/helpers/id_backend.py
+++ id/helpers/id_backend.py
@@ -1,28 +1,152 @@
-''' Handles the ID assignment/collision operations. 
+''' Public facing entry point for ID backend processing/generating/assignment.
 
 Public functions:
-    id_assign: Goes through the ID assign process for the passed document. 
-    get_record_by_id: Gets the record by the biomarker ID.
+    process_file_data: Processes the data objects for a file.
+    get_record_by_id: Gets record(s) by the biomarker ID.
 
 Private functions:
-
+    _id_assign: Goes through the complete ID assign process for an incoming data record.
 '''
 
-import sys
-import hashlib
-import re
-import pymongo
 from pymongo.database import Database
-import logging 
-import misc_functions as misc_fns
 from typing import Union
+import os
+import logging
 import canonical_helpers as canonical
 import second_level_helpers as second
+import datetime
+import misc_functions as misc_fn
+import deepdiff as dd # type: ignore
 
 CANONICAL_DEFAULT = canonical.CANONICAL_DEFAULT
 SECOND_DEFAULT = second.SECOND_DEFAULT
+DATA_DEFAULT = 'biomarker_collection'
+UNREVIEWED_DEFAULT = 'unreviewed_collection'
 
-def id_assign(document: dict, dbh: Database, canonical_id_collection: str = CANONICAL_DEFAULT, second_id_collection: str = SECOND_DEFAULT) -> tuple:
+def process_file_data(data: list,
+                      dbh: Database,
+                      filepath: str,
+                      canonical_id_local_path: str,
+                      second_id_local_path: str,
+                      data_coll: str = DATA_DEFAULT,
+                      unreviewed_coll: str = UNREVIEWED_DEFAULT,
+                      can_id_coll: str = CANONICAL_DEFAULT,
+                      second_id_coll: str = SECOND_DEFAULT,
+                      ) -> list:
+    ''' Processes the data for ID assignments.
+    
+    Parameters
+    ----------
+    data: list
+        The list of data objects for the current file.
+    dbh: Database
+        The database handle.
+    filepath: str
+        The filepath of the current file being processed.
+    canonical_id_local_path: str
+        The filepath to the local ID map. 
+    second_id_local_path: str 
+        The filepath to the local second level ID map.
+    data_coll: str 
+        The main data collection.
+    unreviewed_coll: str
+        The unreviewed data colelction.
+    can_id_coll: str
+        The canonical ID map collection.
+    second_id_coll: str
+        The second level ID map collection.
+
+    Returns
+    -------
+    list
+        Returns the updated data list with the ID values assigned.   
+    '''
+    if not data:
+        logging.error(f'No data found for `{filepath}`.')
+        print(f'No data found for `{filepath}`.')
+        return []
+
+    updated_data: list[dict] = []
+    collision_filepath = f'./collision_reports/{os.path.splitext(os.path.split(filepath)[1])[0]}_collisions.json'
+    collisions: dict = {}
+    # standard collisions are second level ID collisions where the new record differs somewhat from the 
+    # existing record 
+    standard_collision_count = 0
+    # hard collisions are second level ID collisions where the new record matches the existing record
+    # completely or is an entire subset of the existing record
+    hard_collision_count = 0
+
+    for idx, document in enumerate(data):
+
+        canonical_id, second_level_id, second_level_collision, hash_value, core_str = _id_assign(document, dbh, can_id_coll, second_id_coll) 
+        document['biomarker_canonical_id'] = canonical_id
+        document['biomarker_id'] = second_level_id
+
+        if second_level_collision:
+
+            existing_record = get_record_by_id(second_level_id, False, dbh, data_coll)
+            existing_unreviewed_records = get_record_by_id(second_level_id, False, dbh, unreviewed_coll)
+            base_collision_obj = {
+                'timestamp': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                'IDs': {
+                    'canonical': canonical_id,
+                    'second_level': second_level_id
+                },
+                'filepath': filepath,
+                'hash_value': hash_value,
+                'core_values_str': core_str
+            }
+            reviewed_difference = dd.DeepDiff(document, existing_record, ignore_order = True)
+            unreviewed_object = None
+            if existing_unreviewed_records is not None:
+                unreviewed_differences = [dd.DeepDiff(document, i) for i in existing_unreviewed_records]
+                unreviewed_object = [
+                    {f'collision_{idx}': v} for idx, v in enumerate(unreviewed_differences)
+                ]
+
+            # hard collision
+            if reviewed_difference == {}:
+                hard_collision_count += 1
+                _dict_key = f'HARD_COLLISION_NUM_{hard_collision_count}'
+                collisions[_dict_key] = base_collision_obj
+                collisions[_dict_key]['reviewed_collision_info'] = {
+                    'collision_type': 'hard',
+                    'reviewed_difference': reviewed_difference
+                }
+                collisions[_dict_key]['unreviewed_collisions'] = unreviewed_object if unreviewed_object else []
+                output_message = f'HARD collision detected for record number `{idx}` on IDs\
+                    `{canonical_id}`, `{second_level_id}` in file `{filepath}`.'
+                document['collision'] = 2
+            # soft collision 
+            else: 
+                standard_collision_count += 1
+                _dict_key = f'STANDARD_COLLISION_NUM_{standard_collision_count}'
+                collisions[_dict_key] = base_collision_obj
+                collisions[_dict_key]['reviewed_collision_info'] = {
+                    'collision_type': 'soft',
+                    'reviewed_difference': reviewed_difference
+                }
+                collisions[_dict_key]['unreviewed_collisions'] = unreviewed_object if unreviewed_object else []
+                output_message = f'STANDARD collision detected for record number `{idx}` on IDs\
+                    `{canonical_id}`, `{second_level_id}` in file `{filepath}`.'
+                document['collision'] = 1
+
+            logging.warning(output_message)
+            print(output_message)
+
+        else:
+            document['collision'] = 0
+
+        updated_data.append(document)
+
+    misc_fn.write_json(collision_filepath, collisions)
+    logging.info(f'Finished assigning ID\'s for {filepath}.\n\t\
+        Soft collisions: {standard_collision_count}\n\t\
+        Hard collisions: {hard_collision_count}'
+    )
+    return updated_data
+
+def _id_assign(document: dict, dbh: Database, canonical_id_coll: str = CANONICAL_DEFAULT, second_id_coll: str = SECOND_DEFAULT) -> tuple:
     ''' Goes through the ID assign process for the passed document.
 
     Parameters
@@ -31,24 +155,38 @@ def id_assign(document: dict, dbh: Database, canonical_id_collection: str = CANO
         The document to assign the ID for.
     dbh: Database
         The database handle.
-    id_collection: str (default: 'id_map_collection')
+    can_id_coll: str (default: CANONICAL_DEFAULT)
         The name of the collection to check for hash collisions.
+    second_id_coll: str (default: SECOND_DEFAULT)
     
     Returns
     -------
-    tuple: (str, str, str , bool, str)
-        The assigned canonical biomarker ID, second level ID, hash value, a collision boolean, and the core values string.
+    tuple: (str, str, bool)
+        The assigned canonical biomarker ID, second level ID, collision boolean, hash value, and core values string.
     '''
-    canonical_id, hash_value, core_values_str, canonical_collision = canonical.get_ordinal_id(document, dbh, CANONICAL_DEFAULT)
+    canonical_id, hash_value, core_values_str, canonical_collision = canonical.get_ordinal_id(document, dbh, canonical_id_coll)
     second_level_id, second_level_collision = second.get_second_level_id(
         canonical_id,
         canonical_collision,
         document,
         dbh,
-        SECOND_DEFAULT
+        second_id_coll
     )
+    return canonical_id, second_level_id, second_level_collision, hash_value, core_values_str
 
-def get_record_by_id(biomarker_id: str, canonical_level: str, dbh: Database, db_collection: str) -> Union[dict, None]:
+def _update_local_id_maps(can_id_data: tuple[str, list], second_id_data: tuple[str, list]) -> None:
+    ''' Updates the local storage replica of the canonical and secondary level ID collections.
+
+    Parameters
+    ----------
+    can_id_data: tuple(str, list)
+        The local canonical ID data file path and the data itself to add.
+    second_id_data: tuple(str, list)
+        The local secondary ID data file path and the data itself to add.
+    '''
+    # TODO : determine where to do this, best approach might just be to add wholesale download and upload periodically. 
+    
+def get_record_by_id(biomarker_id: str, canonical_level: bool, dbh: Database, db_collection: str) -> Union[dict, None]:
     ''' Gets the record by the biomarker ID at the specified level.
 
     Parameters
