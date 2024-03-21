ff5dfea0c71101db5bc9c75cfe4290fcc807b5ba
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-03-20 23:52:32 -0400

    add dump id collection logic

--- id/id_assign.py
+++ id/id_assign.py
@@ -5,21 +5,6 @@ import glob
 from helpers import id_backend as id_backend
 from helpers import misc_functions as misc_fns
 
-# batch size for mongodb bulk write operations
-def update_local_id_collection(id_collection_local_path: str, new_id_data: list) -> None:
-    ''' Updates the local storage replica of the id_map_collection.
-
-    Parameters
-    ----------
-    id_collection_local_path: str
-        The filepath to the local storage replica of the id_map_collection.
-    new_id_data: list
-        The new entries to add to the local storage replica of the id_map_collection.
-    '''
-    existing_id_data = misc_fns.load_json(id_collection_local_path)
-    existing_id_data.extend(new_id_data)
-    misc_fns.write_json(id_collection_local_path, existing_id_data)
-
 def main():
 
     ### handle command line arguments
@@ -43,19 +28,19 @@ def main():
     ### get config info for database connection
     config_obj = misc_fns.load_json('../api/config.json')
     if not isinstance(config_obj, dict):
-        print(f"Error reading config JSON, expected type `dict`, got {type(config_obj)}")
+        print(f"Error reading config JSON, expected type `dict`, got {type(config_obj)}.")
         sys.exit(1)
     mongo_port = config_obj['dbinfo']['port'][server]
     host = f'mongodb://127.0.0.1:{mongo_port}'
-    _db_name = config_obj['dbinfo']['dbname']
-    db_user = config_obj['dbinfo'][_db_name]['user']
-    db_pass = config_obj['dbinfo'][_db_name]['password']
+    db_name = config_obj['dbinfo']['dbname']
+    db_user = config_obj['dbinfo'][db_name]['user']
+    db_pass = config_obj['dbinfo'][db_name]['password']
     data_root_path = config_obj['data_path']
-    canonical_id_collection = config_obj['dbinfo'][_db_name]['canonical_id_map']
-    second_level_id_collection = config_obj['dbinfo'][_db_name]['second_level_id_map']
-    data_collection = config_obj['dbinfo'][_db_name]['collection']
-    unreviewed_collection = config_obj['dbinfo'][_db_name]['unreviewed_collection']
-    dbh = misc_fns.get_mongo_handle(host, _db_name, db_user, db_pass)
+    canonical_id_collection = config_obj['dbinfo'][db_name]['canonical_id_map']
+    second_level_id_collection = config_obj['dbinfo'][db_name]['second_level_id_map']
+    data_collection = config_obj['dbinfo'][db_name]['collection']
+    unreviewed_collection = config_obj['dbinfo'][db_name]['unreviewed_collection']
+    dbh = misc_fns.get_mongo_handle(host, db_name, db_user, db_pass)
 
     ### setup logger
     misc_fns.setup_logging('./logs/id_assign.log')
@@ -67,8 +52,6 @@ def main():
 
     ### initiate id assignment logic 
     data_release_glob_pattern = f'{data_root_path}/generated/datamodel/new_data/current/*.json'
-    canonical_id_collection_local_path = f'{data_root_path}/generated/datamodel/canonical_id_collection.json'
-    second_level_id_collection_local_path = f'{data_root_path}/generated/datamodel/second_level_id_collection.json'
 
     for fp in glob.glob(data_release_glob_pattern):
         data = misc_fns.load_json(fp)
@@ -78,8 +61,6 @@ def main():
             continue
         updated_data = id_backend.process_file_data(
             data, dbh, fp, 
-            canonical_id_collection_local_path,
-            second_level_id_collection_local_path,
             data_collection, unreviewed_collection, 
             canonical_id_collection, second_level_id_collection, 
         )
@@ -88,7 +69,19 @@ def main():
             print(f"Error reading data from file `{fp}`, expected `list`, got `{type(data)}`. Skipping...")
             continue
         misc_fns.write_json(fp, updated_data)
-        
+    
+    canonical_id_collection_local_path = f'{data_root_path}/generated/datamodel/canonical_id_collection.json'
+    second_level_id_collection_local_path = f'{data_root_path}/generated/datamodel/second_level_id_collection.json'
+    connection_string = misc_fns.create_connection_string(host, db_user, db_pass, db_name)
+    if id_backend.dump_id_collection(connection_string, canonical_id_collection_local_path, canonical_id_collection):
+        print('Successfully dumped canonical ID map.') 
+    else:
+        print('Failed dumping canonical ID map. You will have to update manually.')
+    if id_backend.dump_id_collection(connection_string, second_level_id_collection_local_path, second_level_id_collection):
+        print('Successfully dumped second level ID map.') 
+    else:
+        print('Failed dumping second level ID map. You will have to update manually.')
+
     logging.info('Finished ID assignment process ---------------------')
         
 if __name__ == '__main__':
