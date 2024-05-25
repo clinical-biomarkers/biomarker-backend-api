edc4b5ca4ef3010668790c9d3b1cbf0e46bd7eb0
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-05-25 14:39:48 -0400

    return user facing error object directly from log error

--- api/biomarker/backend_utils/db.py
+++ api/biomarker/backend_utils/db.py
@@ -12,7 +12,7 @@ from . import (
     DB_COLLECTION,
     SEARCH_CACHE_COLLECTION,
 )
-from typing import Optional, Dict, cast, Tuple, Union, List
+from typing import Optional, Dict, cast, Tuple, Union, List, Any
 import datetime
 import pytz  # type: ignore
 import string
@@ -25,7 +25,7 @@ from pymongo.errors import PyMongoError
 
 def log_request(
     request_object: Optional[Dict], endpoint: str, api_request: Request
-) -> Tuple[int, Optional[str]]:
+) -> Optional[Dict[Any, Any]]:
     """Logs an API request in the request log collection.
 
     Parameters
@@ -39,16 +39,15 @@ def log_request(
 
     Returns
     -------
-    tuple : (int, str or None)
-        The exit status; 0 for success, 1 for failure to log. If failure, also
-        returns the error ID.
+    str or None
+        None on success, the error ID on error.
     """
     if request_object and len(json.dumps(request_object)) > REQ_LOG_MAX_LEN:
-        _, error_id = log_error(
+        error_obj = log_error(
             error_msg=f"Request object length exceeds REQ_LOG_MAX_LEN ({REQ_LOG_MAX_LEN})",
             origin="log_request",
         )
-        return (1, error_id)
+        return error_obj
 
     header_dict = {
         "user_agent": api_request.headers.get("User-Agent"),
@@ -70,30 +69,32 @@ def log_request(
 
     try:
         dbh[REQ_LOG_COLLECTION].insert_one(log_object)
-        return (0, None)
     except Exception as e:
-        _, error_id = log_error(
+        error_obj = log_error(
             error_msg=f"Failed to log request.\n{e}", origin="log_request"
         )
-        return (1, error_id)
+        return error_obj
+
+    return None
 
 
-def log_error(error_msg: str, origin: str) -> Tuple[int, Optional[str]]:
+def log_error(error_log: str, error_msg: str, origin: str, **kwargs) -> Dict:
     """Logs an error in the error collection log.
 
     Parameters
     ----------
-    error_msg : str
+    error_log : str
         The error message to log (a traceback stack trace or custom
         error message).
+    error_msg : str
+        User facing error message.
     origin : str
         The function calling this function.
 
     Returns
     -------
-    tuple : (int, str or None)
-        The exit status; 0 for success, 1 for failure to log. If success, also
-        returns the error ID.
+    dict
+        The return JSON.
     """
 
     def _create_error_id(
@@ -118,6 +119,7 @@ def log_error(error_msg: str, origin: str) -> Tuple[int, Optional[str]]:
     error_id = _create_error_id()
     error_object = {
         "id": error_id,
+        "log": error_log,
         "msg": error_msg,
         "origin": origin,
         "timestamp": create_timestamp(),
@@ -126,19 +128,46 @@ def log_error(error_msg: str, origin: str) -> Tuple[int, Optional[str]]:
     dbh = custom_app.mongo_db
     try:
         dbh[ERROR_LOG_COLLECTION].insert_one(error_object)
-        return (0, error_id)
+        custom_app.api_logger.info(error_object)
     except Exception as e:
         custom_app.api_logger.error(
             f"Failed to log error.\n{e}\nError object: {error_object}"
         )
-        return (1, None)
+    return _create_error_obj(error_id, error_msg, **kwargs)
+
+
+def _create_error_obj(error_id: str, error_msg: str, **kwargs: Any) -> Dict[Any, Any]:
+    """Creates a standardized error object.
+
+    Parameters
+    ----------
+    error_id : str
+        The error ID.
+    error_msg : str
+        The standardized error message/code.
+    extra_info : str or None
+        Supplementary error information, if available.
+
+    Returns
+    -------
+    dict
+        The error object.
+    """
+    error_object: Dict[Any, Any] = {
+        "error": {
+            "error_id": error_id,
+            "error_msg": error_msg,
+        }
+    }
+    error_object["error"].update(kwargs)
+    return error_object
 
 
 def find_one(
     query_object: Dict,
     projection_object: Dict = {"_id": 0},
     collection: str = DB_COLLECTION,
-) -> Tuple[int, Union[Dict, str, None]]:
+) -> Tuple[Dict[Any, Any], int]:
     """Performs a find_one query on the specified collection.
 
     Parameters
@@ -153,29 +182,36 @@ def find_one(
 
     Returns
     -------
-    tuple : (int, dict str or None)
-        The status code and the found document or None if no document was found
-        or an error occurred. Or the error ID on non-success exit code. 0 for
-        successful query (dict can still be None if no document was found), 1 for
-        caught exception.
+    tuple : (dict, int)
+        The retrieved document or error object and the HTTP status code.
     """
     custom_app = cast_app(current_app)
     dbh = custom_app.mongo_db
     try:
         result = dbh[collection].find_one(query_object, projection_object)
     except PyMongoError as db_error:
-        _, error_id = log_error(
-            error_msg=f"PyMongoError querying database during find_one.\n{db_error}",
+        error_obj = log_error(
+            error_log=f"PyMongoError querying database during find_one.\n{db_error}",
+            error_msg="internal-database-error",
             origin="find_one",
         )
-        return 1, error_id
+        return error_obj, 500
     except Exception as e:
-        _, error_id = log_error(
-            error_msg=f"Non-PyMongoError querying database during find_one.\n{e}",
+        error_obj = log_error(
+            error_log=f"Non-PyMongoError querying database during find_one.\n{e}",
+            error_msg="internal-database-error",
             origin="find_one",
         )
-        return 1, error_id
-    return 0, result
+        return error_obj, 500
+    # no matching document
+    if result is None:
+        error_obj = log_error(
+            error_log=f"Query object: {query_object} not found.\nProjection object: {projection_object}",
+            error_msg="record-not-found",
+            origin="find_one",
+        )
+        return error_obj, 404
+    return result, 200
 
 
 def create_timestamp() -> str:
