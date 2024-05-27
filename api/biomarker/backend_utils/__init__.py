5d0151055217c01e9ac8d5acdf1d24cba73d02c9
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-05-27 16:14:09 -0400

    add cache batch size constant

--- api/biomarker/backend_utils/__init__.py
+++ api/biomarker/backend_utils/__init__.py
@@ -3,5 +3,6 @@ SEARCH_CACHE_COLLECTION = "search_cache"
 REQ_LOG_COLLECTION = "request_log_collection"
 REQ_LOG_MAX_LEN = 20_000
 ERROR_LOG_COLLECTION = "error_log_collection"
+CACHE_BATCH_SIZE = 5_000
 TIMESTAMP_FORMAT = "%Y-%m-%d %H:%M:%S %Z%z"
 TIMEZONE = "US/Eastern"
