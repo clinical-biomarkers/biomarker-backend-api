477e4c16b1308a29a98477aec49d1c9c49dfdcd0
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-06-11 18:51:59 -0400

    more log format tweaking

--- api/biomarker/backend_utils/db.py
+++ api/biomarker/backend_utils/db.py
@@ -216,11 +216,9 @@ def execute_pipeline(
 
         # TODO : delete logging
         custom_app.api_logger.info("********************************** Pipeline Log **********************************")
-        custom_app.api_logger.info("PIPELINE:")
-        custom_app.api_logger.info(f"{pipeline}\n")
+        custom_app.api_logger.info(f"PIPELINE:\n{pipeline}\n")
         explain_output = dbh.command("aggregate", collection, pipeline=pipeline, explain=True)
-        custom_app.api_logger.info("COMMAND EXPLAIN OUTPUT:")
-        custom_app.api_logger.info(f"{explain_output}\n")
+        custom_app.api_logger.info(f"COMMAND EXPLAIN OUTPUT:\n{explain_output}\n")
 
         return result, 200
     except PyMongoError as db_error:
