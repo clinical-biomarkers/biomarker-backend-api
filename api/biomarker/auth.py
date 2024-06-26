975dfa65569dcbfb2dbed5f8c365d6c58f3c6f02
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-06-26 16:56:43 -0400

    add contact endpoint

---
+++ api/biomarker/auth.py
@@ -0,0 +1,14 @@
+from flask import request
+from flask_restx import Resource, Namespace  # type: ignore
+from .backend_utils import auth_utils as auth_utils
+
+api = Namespace("auth", description="Authentication API namespace.")
+
+class Contact(Resource):
+
+    @api.doc("contact")
+    def post(self):
+        return 
+    
+    def get(self):
+        return self.post()
---
+++ api/biomarker/backend_utils/auth_utils.py
@@ -0,0 +1,62 @@
+""" Handles the backend logic for the biomarker auth endpoints.
+"""
+
+from flask import Request
+from typing import Tuple, Dict
+from dotenv import load_dotenv
+import os
+import traceback
+import smtplib
+from email.mime.text import MIMEText
+
+from . import utils as utils
+from . import db as db_utils
+from . import CONTACT_SOURCE, CONTACT_RECIPIENTS
+
+
+def contact(api_request: Request) -> Tuple[Dict, int]:
+    """Entry point for the backend logic of the contact endpoint.
+
+    Parameters
+    ----------
+    tuple : (dict, int)
+        The return JSON and HTTP code.
+    """
+    request_arguments, request_http_code = utils.get_request_object(
+        api_request, "contact"
+    )
+    if request_http_code != 200:
+        return request_arguments, request_http_code
+
+    response_txt = f"\n\n{request_arguments['fname']},\n"
+    response_txt += "We have received your message and will make every effort to respond to you within a reasonable amount of time."
+    response_json = {"type": "alert-success", "message": response_txt}
+
+    load_dotenv()
+    source_app_password = os.environ.get("EMAIL_APP_PASSWORD")
+    if source_app_password is None:
+        error_obj = db_utils.log_error(
+            error_log="Error reading email password environment variable.",
+            error_msg="internal-email-error",
+            origin="contact",
+        )
+        return error_obj, 500
+
+    msg = MIMEText(request_arguments["message"])
+    msg["Subject"] = request_arguments["subject"]
+    msg["To"] = ", ".join(CONTACT_RECIPIENTS)
+    msg["From"] = f"{CONTACT_SOURCE}@gmail.com"
+
+    try:
+        smtp_server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        smtp_server.login(user=CONTACT_SOURCE, password=source_app_password)
+        smtp_server.sendmail(msg["From"], CONTACT_RECIPIENTS, msg.as_string())
+        smtp_server.quit()
+        return response_json, 200
+    except Exception as e:
+        error_obj = db_utils.log_error(
+            error_log=f"Failure to send contact email. {e}\n{traceback.format_exc()}",
+            error_msg="internal-email-error",
+            origin="contact",
+        )
+        return error_obj, 500
