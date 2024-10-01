a5d9111acd038d4b2a7c8b826b0962e4f19aa846
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-09-30 20:52:37 -0400

    Add index building and id collection loading

--- load/load_data.py
+++ load/load_data.py
@@ -15,7 +15,12 @@ import time
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from tutils.db import get_standard_db_handle
+from tutils.db import (
+    get_standard_db_handle,
+    get_connection_string,
+    setup_index,
+    create_text_index,
+)
 from tutils.config import get_config
 from tutils.general import load_json_type_safe, resolve_symlink, get_user_confirmation
 from tutils.parser import standard_parser
@@ -25,6 +30,7 @@ from load.load_utils import (
     create_load_record_command,
     bulk_load,
     process_stats,
+    load_id_collection,
 )
 from load.preprocess import CHECKPOINT_VAL
 
@@ -54,6 +60,8 @@ def main() -> None:
     data_root_path_segment = config_obj["data_path"]
     generated_path_segment = config_obj["generated_path_segment"]
     merged_data_path_segment = config_obj["merged_data_segment"]
+    canonical_id_collection = config_obj["dbinfo"][db_name]["canonical_id_map"]
+    second_level_id_collection = config_obj["dbinfo"][db_name]["second_level_id_map"]
 
     merged_path_root = os.path.join(
         data_root_path_segment, *generated_path_segment, *merged_data_path_segment
@@ -69,6 +77,87 @@ def main() -> None:
     collision_data_files = glob.glob(collision_data_pattern)
 
     dbh = get_standard_db_handle(server)
+
+    paths = [
+        "biomarker_component.biomarker",
+        "biomarker_component.assessed_biomarker_entity.recommended_name",
+        "biomarker_component.assessed_biomarker_entity_id",
+        "biomarker_component.assessed_entity_type",
+        "condition.recommended_name.name",
+        "best_biomarker_role.role",
+    ]
+    log_msg(logger=LOGGER, msg="Attempting to create indexes...")
+    setup_index(
+        collection=dbh[biomarker_collection],
+        index_field="biomarker_id",
+        unique=True,
+        index_name="biomarker_id_1",
+        logger=LOGGER,
+    )
+    for path in paths:
+        setup_index(
+            collection=dbh[biomarker_collection],
+            index_field=path,
+            index_name=f"{path}_1",
+            unique=False,
+            order="ascending",
+        )
+        setup_index(
+            collection=dbh[biomarker_collection],
+            index_field=path,
+            index_name=f"{path}_-1",
+            unique=False,
+            order="descending",
+        )
+    create_text_index(collection=dbh[biomarker_collection], logger=LOGGER)
+
+    if server != "dev":
+        canonical_id_collection_local_path = os.path.join(
+            data_root_path_segment,
+            *generated_path_segment,
+            "canonical_id_collection.json",
+        )
+        second_level_id_collection_local_path = os.path.join(
+            data_root_path_segment,
+            *generated_path_segment,
+            "second_level_id_collection.json",
+        )
+        connection_string = get_connection_string(server=server)
+        if load_id_collection(
+            connection_string=connection_string,
+            load_path=canonical_id_collection_local_path,
+            collection=canonical_id_collection,
+        ):
+            log_msg(
+                logger=LOGGER,
+                msg="Successfully loaded canonical ID map.",
+                to_stdout=True,
+            )
+        else:
+            log_msg(
+                logger=LOGGER,
+                msg="Failed loading canonical ID map. You will have to update manually.",
+                level="error",
+                to_stdout=True,
+            )
+        if load_id_collection(
+            connection_string=connection_string,
+            load_path=second_level_id_collection_local_path,
+            collection=second_level_id_collection,
+        ):
+            log_msg(
+                logger=LOGGER,
+                msg="Successfully loaded second level ID map.",
+                to_stdout=True,
+            )
+        else:
+            log_msg(
+                logger=LOGGER,
+                msg="Failed loading second level ID map. You will have to update manually.",
+                level="error",
+                to_stdout=True,
+            )
+
     log_msg(logger=LOGGER, msg="Clearing collections...")
     clear_collection_start_time = time.time()
     clear_collections(dbh=dbh)
