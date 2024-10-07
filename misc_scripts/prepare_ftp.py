a28b20c77a5bbc7d3983dd695460ef209dfaf1f3
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-10-06 21:27:19 -0400

    Refactor create ftp release

--- misc_scripts/prepare_ftp.py
+++ misc_scripts/prepare_ftp.py
@@ -8,6 +8,13 @@ from tutils.config import get_config
 from tutils.parser import standard_parser, parse_server
 from tutils.general import copy_file, resolve_symlink, get_user_confirmation
 
+ALL_BIOMARKER_JSON = "all-biomarker-json"
+ALL_BIOMARKER_TSV = "all-biomarker-tsv"
+ALL_BIOMARKER_JSON_MERGED = "all-biomarker-json-merged"
+
+TAR_EXT = ".tar.gz"
+TAR_CMD = "tar -czvf"
+
 
 def main() -> None:
 
@@ -33,51 +40,47 @@ def main() -> None:
     )
     tsv_dir = "/data/shared/biomarkerdb/releases/data/current"
 
-    existing_data_resolved_symlink = resolve_symlink(existing_data_dir)
-    merged_data_resolved_symlink = resolve_symlink(merged_data_dir)
-    tsv_data_resolved_symlink = resolve_symlink(tsv_dir)
+    data_config: dict[str, dict[str, str]] = {
+        "json": {
+            "resolved_symlink": resolve_symlink(existing_data_dir) or "UNABLE TO RESOLVE SYMLINK",
+            "src_glob_pattern": os.path.join(existing_data_dir, "*.json"),
+            "dest_path": os.path.join(ftp_path, ALL_BIOMARKER_JSON),
+            "tarball": os.path.join(ftp_path, f"{ALL_BIOMARKER_JSON}{TAR_EXT}"),
+        },
+        "tsv": {
+            "resolved_symlink": resolve_symlink(tsv_dir) or "UNABLE TO RESOLVE SYMLINK",
+            "src_glob_pattern": os.path.join(tsv_dir, "reviewed", "*"),
+            "dest_path": os.path.join(ftp_path, ALL_BIOMARKER_JSON_MERGED),
+            "tarball": os.path.join(ftp_path, f"{ALL_BIOMARKER_TSV}{TAR_EXT}"),
+        },
+        "merged": {
+            "resolved_symlink": resolve_symlink(merged_data_dir) or "UNABLE TO RESOLVE SYMLINK",
+            "src_glob_pattern": os.path.join(merged_data_dir, "merged_json", "*.json"),
+            "dest_path": os.path.join(ftp_path, ALL_BIOMARKER_JSON_MERGED),
+            "tarball": os.path.join(ftp_path, f"{ALL_BIOMARKER_JSON_MERGED}{TAR_EXT}"),
+        },
+    }
 
-    existing_data_glob_pattern = os.path.join(existing_data_dir, "*.json")
-    merged_data_glob_pattern = os.path.join(merged_data_dir, "merged_json", "*.json")
-    tsv_glob_pattern = os.path.join(tsv_dir, "reviewed", "*")
-
-    existing_data_dest_path = os.path.join(ftp_path, "all-biomarker-json")
-    merged_data_dest_path = os.path.join(ftp_path, "all-biomarker-json-merged")
-    tsv_dest_path = os.path.join(ftp_path, "all-biomarker-tsv")
-
-    confirmation_str = "Copying files from:"
-    confirmation_str += f"\n\t1. {existing_data_glob_pattern}"
-    confirmation_str += f"\n\t\t Resolved symlink: {existing_data_resolved_symlink}"
-    confirmation_str += f"\n\t2. {merged_data_glob_pattern}"
-    confirmation_str += f"\n\t\t Resolved symlink: {merged_data_resolved_symlink}"
-    confirmation_str += f"\n\t3. {tsv_glob_pattern}"
-    confirmation_str += f"\n\t\t Resolved symlink: {tsv_data_resolved_symlink}"
-    confirmation_str += "Copying to:"
-    confirmation_str += f"\n\t1. {existing_data_dest_path}"
-    confirmation_str += f"\n\t2. {merged_data_dest_path}"
-    confirmation_str += f"\n\t3. {tsv_dest_path}"
+    confirmation_str = "Confirmation data:"
+    for idx, (data_type, metadata) in enumerate(data_config.items()):
+        confirmation_str += f"\n\t{idx}. {data_type}"
+        confirmation_str += f"\n\t\tResolved symlink: {metadata['resolved_symlink']}"
+        confirmation_str += f"\n\t\tSource glob pattern: {metadata['src_glob_pattern']}"
+        confirmation_str += f"\n\t\tDestination: {metadata['dest_path']}"
+        confirmation_str += f"\n\t\tTarball: {metadata['tarball']}"
     print(confirmation_str)
     get_user_confirmation()
 
-    if not os.path.isdir(existing_data_dest_path):
-        os.mkdir(existing_data_dest_path)
-    existing_data_files_to_copy = glob.glob(existing_data_glob_pattern)
-    for fp in existing_data_files_to_copy:
-        copy_file(src=fp, dest=existing_data_dest_path)
-
-    if not os.path.isdir(merged_data_dest_path):
-        os.mkdir(merged_data_dest_path)
-    merged_data_files_to_copy = glob.glob(merged_data_glob_pattern)
-    for fp in merged_data_files_to_copy:
-        copy_file(src=fp, dest=merged_data_dest_path)
-
-    if not os.path.isdir(tsv_dest_path):
-        os.mkdir(tsv_dest_path)
-    tsv_files_to_copy = glob.glob(tsv_glob_pattern)
-    for fp in tsv_files_to_copy:
-        copy_file(src=fp, dest=tsv_dest_path)
-
-    # TODO : Create tarballs
+    for data_type, metadata in data_config.items():
+        if not os.path.isdir(metadata["dest_path"]):
+            os.mkdir(metadata["dest_path"])
+        files_to_copy = glob.glob(metadata["src_glob_pattern"])
+        for fp in files_to_copy:
+            copy_file(src=fp, dest=metadata["dest_path"])
+        subprocess.run(f"{TAR_CMD} {metadata['tarball']} {metadata['dest_path']}", shell=True)
+        if data_type == "merged":
+            subprocess.run(f"rm -r {metadata['dest_path']}", shell=True)
+    
 
 if __name__ == "__main__":
     main()
