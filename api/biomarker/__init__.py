a17419f5969098a9345b39ac33d9348266fa96ac
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-10-13 15:23:57 -0400

    Remove default specs

--- api/biomarker/__init__.py
+++ api/biomarker/__init__.py
@@ -87,6 +87,7 @@ def create_app():
         version="1.0",
         title="Biomarker APIs",
         description="Biomarker Knowledgebase API",
+        add_specs=False
     )
 
     @api.route("/swagger.json")
