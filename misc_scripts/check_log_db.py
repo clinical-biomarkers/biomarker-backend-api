da5ce8c844c1f781fe701ca112b3bca2e9180ab4
Sean Kim <33474168+seankim658@users.noreply.github.com>
2025-01-03 14:42:07 -0500

    Add ability to ignore specific endpoint

--- misc_scripts/check_log_db.py
+++ misc_scripts/check_log_db.py
@@ -1,14 +1,16 @@
 """Peak into the sqlite log database.
 
-usage: parser.py [-h] server table limit
+usage: parser.py [-h] [-i IGNORE_ENDPOINT] [-l LIMIT] server table
 
 positional arguments:
-  server      prd/beta/tst/dev
-  table       api/frontend
-  limit
+  server                prd/beta/tst/dev
+  table                 api/frontend
 
 options:
-  -h, --help  show this help message and exit
+  -h, --help            show this help message and exit
+  -i IGNORE_ENDPOINT, --ignore-endpoint IGNORE_ENDPOINT
+                        Ignore rows with specified endpoint value
+  -l LIMIT, --limit LIMIT
 """
 
 import sqlite3
@@ -25,11 +27,19 @@ from tutils.config import get_config
 def main():
     parser, server_list = standard_parser()
     parser.add_argument("table", help="api/frontend")
+    parser.add_argument(
+        "-i",
+        "--ignore-endpoint",
+        type=str,
+        default=None,
+        help="Ignore rows with specified endpoint value",
+    )
     parser.add_argument("-l", "--limit", type=int, default=5)
     options = parser.parse_args()
 
     server = parse_server(parser=parser, server=options.server, server_list=server_list)
     table = options.table.lower().strip()
+    ignore_endpoint = options.ignore_endpoint
     limit = options.limit
     if table not in {"api", "frontend"}:
         print("Invalid table.")
@@ -47,7 +57,16 @@ def main():
     columns = [col[1] for col in cursor.fetchall()]
 
     # get latest rows
-    cursor.execute(f"SELECT * FROM {table} ORDER BY id DESC LIMIT {limit}")
+    query = f"SELECT * FROM {table}"
+    if ignore_endpoint:
+        query += " WHERE endpoint != ?"
+    query = " ORDER BY id DESC LIMIT ?"
+
+    if ignore_endpoint:
+        cursor.execute(query, (ignore_endpoint, limit))
+    else:
+        cursor.execute(query, (limit,))
+
     rows = cursor.fetchall()
     rows = reversed(rows)
 
