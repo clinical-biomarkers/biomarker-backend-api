c261850a64191740c40f8ddc6bbe0b3ed634fdb1
Sean Kim <33474168+seankim658@users.noreply.github.com>
2025-02-21 13:43:25 -0500

    Add logging for success

--- misc_scripts/notify.py
+++ misc_scripts/notify.py
@@ -2,24 +2,31 @@
 
 import requests
 import json
-from argparse import ArgumentParser
 import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from tutils.logging import setup_logging, log_msg
+from tutils.parser import standard_parser, parse_server
+from tutils.config import get_config
 
 LOGGER = setup_logging("notification.log")
-ENDPOINT = "http://localhost:8089/auth/contact_notification"
+ENDPOINT = "http://localhost:{port}/auth/contact_notification"
 
 
 def main():
-    parser = ArgumentParser()
+    parser, server_list = standard_parser()
     parser.add_argument("--email", nargs="+", required=True)
     parser.add_argument("--subject", type=str, required=True)
     parser.add_argument("--message", type=str, required=True)
-
     options = parser.parse_args()
+    server = parse_server(parser=parser, server=options.server, server_list=server_list)
+
+    config_obj = get_config()
+    port = config_obj["api_port"][server]
+
+    api_endpoint = ENDPOINT.format(port=port)
+
     payload = {
         "email": options.email,
         "subject": options.subject,
@@ -27,7 +34,7 @@ def main():
     }
 
     try:
-        response = requests.post(ENDPOINT, json=payload)
+        response = requests.post(api_endpoint, json=payload)
         if response.status_code != 200:
             msg = (
                 f"Non-200 status code: {response.status_code}\n"
@@ -36,6 +43,13 @@ def main():
                 f"\tEmails: {options.email}\n\tSubject: {options.subject}\n\tMessage: {options.message}"
             )
             log_msg(logger=LOGGER, msg=msg, level="error")
+            sys.exit(1)
+
+        msg = (
+            f"Notification success, status code: {response.status_code}\n"
+            f"Args: `{server}`, `{options.email}`, `{options.subject}`, `{options.message}`"
+        )
+        log_msg(logger=LOGGER, msg=msg)
     except Exception as e:
         log_msg(
             logger=LOGGER,
